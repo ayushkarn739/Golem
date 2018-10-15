cdc7766d58999f82054491e444ee1ab219419351
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-10-15 18:56:02 +0530

    Fixed package names

--- FileSearch.go
+++ FileSearch.go
@@ -1,4 +1,4 @@
-package FileSearch
+package main
 
 import (
 	"os"
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fileSearch"
 	"fmt"
 	"os"
 )
 
 func main() {
-	files := fileSearch.Find(os.Args[1])
+	files := Find(os.Args[1])
 	fmt.Println(files)
 }
