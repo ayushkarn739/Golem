98aa0428446b20c1203d978b1ba270e62cfec3bc
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-10-15 09:36:49 +0530

    module for file search

---
+++ src/fileSearch/fileSearch.go
@@ -0,0 +1,23 @@
+package fileSearch
+
+import (
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func Find(filePath string) []string {
+	var files []string
+	re, _ := regexp.Compile(`([a-zA-Z0-9\s_\\.\-\(\):])+(.avi|.flv|.wmv|.mkv|.mp4)$`)
+	err := filepath.Walk(filePath, func(path string, info os.FileInfo, err error) error {
+		b := re.MatchString(path)
+		if b {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
